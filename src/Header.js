fde1519cd1f7c688802a81a59b50c2ae4c9c66a2
Wayne Lai <waynelai01011231@gmail.com>
2022-02-05 17:42:45 -0800

    Fix link hover and change paragraph link color

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <div className="nav-link"><a href="https://uwctf.cs.washington.edu/BatmansKitchen">Wiki</a></div>
       <div className="nav-link"><a href="https://mailman.cs.washington.edu/mailman/listinfo/uwctf">Mailing List</a></div>
       <div className="nav-link"><a href="/writeups">Writeups</a></div>
-      <div className="nav-link"><a href="/#about-us">About us</a></div>
+      <div className="nav-link"><a href="/#about-us">About Us</a></div>
     </div>
   );
 }
