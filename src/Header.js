e94b71f38b0f686b3849d1c7d4570d33b3980bb9
Wayne Lai <waynelai01011231@gmail.com>
2022-02-05 17:48:48 -0800

    Fix nav link not covering whole area

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,10 @@ function Header() {
   return (
     <div className="Header">
       <a href='/#front-page'><img src={logo} id="logo" alt="logo" height = "70px" width="70px"/></a>
-      <div className="nav-link"><a href="https://uwctf.cs.washington.edu/BatmansKitchen">Wiki</a></div>
-      <div className="nav-link"><a href="https://mailman.cs.washington.edu/mailman/listinfo/uwctf">Mailing List</a></div>
-      <div className="nav-link"><a href="/writeups">Writeups</a></div>
-      <div className="nav-link"><a href="/#about-us">About Us</a></div>
+      <a href="https://uwctf.cs.washington.edu/BatmansKitchen"><div className="nav-link">Wiki</div></a>
+      <a href="https://mailman.cs.washington.edu/mailman/listinfo/uwctf"><div className="nav-link">Mailing List</div></a>
+      <a href="/writeups"><div className="nav-link">Writeups</div></a>
+      <a href="/#about-us"><div className="nav-link">About Us</div></a>
     </div>
   );
 }
